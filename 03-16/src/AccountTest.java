3ef5d1f2049eb4dabb6915fd3b876443d2118e13
USER <USER@INET-23>
2018-03-16 15:41:38 +0900

    ...

--- 03-16/src/AccountTest.java
+++ 03-16/src/AccountTest.java
@@ -4,11 +4,14 @@ public class AccountTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a;
+		String b;
 		Scanner scan = new Scanner(System.in);
 		Account obj = new Account();
-		System.out.println("잔액을 입력해주세요.");
+		System.out.println("잔액을 입력해주세요 : ");
 		a = scan.nextInt();
-		obj.setName("tom");
+		System.out.println("이름을 입력해주세요 : ");
+		b = scan.next();
+		obj.setName(b);
 		obj.setBalance(a);
 		if (obj.getBalance() <= 0)
 			System.out.println("오류");
